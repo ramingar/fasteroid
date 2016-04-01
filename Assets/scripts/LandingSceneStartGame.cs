70dd07511782bc2b7b69c8cba8954a469e4861f7
Rafael Minguet <rminguet@gmail.com>
2016-04-01 23:40:09 +0200

    Landing scene to start the game

---
+++ Assets/scripts/LandingSceneStartGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class LandingSceneStartGame : MonoBehaviour {
+	
+    public void startGame ()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Scene001");
+    }
+}

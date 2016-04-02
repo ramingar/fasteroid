eef5045a440f0d67f75a982f3f2ac1474ff9a5e8
Rafael Minguet <rminguet@gmail.com>
2016-04-02 16:33:33 +0200

    Refactoring

---
+++ Assets/scripts/BulletExplossion.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletExplossion : MonoBehaviour {
+
+    void Start ()
+    {
+        InvokeRepeating("explode", 3f, 0f);
+    }
+
+    private void explode ()
+    {
+        this.gameObject.transform.localScale.Scale(this.gameObject.transform.localScale);
+    }
+}
---
+++ Assets/scripts/GoToPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToPlayer : MonoBehaviour {
+
+    [SerializeField]
+    private float velocity = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("Player"))
+        {
+            this.movement();
+        }
+    }
+
+    private void movement()
+    {
+        Vector3 positionPlayer = GameObject.Find("Player").gameObject.transform.position;
+        this.gameObject.transform.position = new Vector3(
+            Mathf.Lerp(this.gameObject.transform.position.x, positionPlayer.x, velocity * Time.deltaTime),
+            Mathf.Lerp(this.gameObject.transform.position.y, positionPlayer.y, velocity * Time.deltaTime),
+            0
+        );
+    }
+}

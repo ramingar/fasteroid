be432fd7ce62fd8db89558f503838817fb4c3af7
Rafael Minguet <rminguet@gmail.com>
2016-04-02 17:33:35 +0200

    Random movement

---
+++ Assets/scripts/MovementRandom.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementRandom : MonoBehaviour {
+
+    [SerializeField]
+    private float velocity = 1.0f;
+
+    private Vector3 positionTarget = new Vector3(0f, 0f, 0f);
+
+    // Use this for initialization
+    void Start ()
+    {
+        InvokeRepeating("changeDirection", 0.5f, 2.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("Player"))
+        {
+            this.movement();
+        }
+    }
+
+    private void movement()
+    {
+        this.gameObject.transform.position = new Vector3(
+            Mathf.Lerp(this.gameObject.transform.position.x, positionTarget.x, velocity * Time.deltaTime),
+            Mathf.Lerp(this.gameObject.transform.position.y, positionTarget.y, velocity * Time.deltaTime),
+            0
+        );
+    }
+
+    private void changeDirection ()
+    {
+        this.positionTarget = Camera.main.ScreenToWorldPoint(new Vector3(
+             Random.Range(0, Camera.main.pixelWidth),
+             Random.Range(0, Camera.main.pixelHeight),
+             0
+        ));
+    }
+}
